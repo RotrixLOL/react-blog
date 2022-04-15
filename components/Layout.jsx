52ed71d4d707c2d12acd20a88472841c83e8a9e9
RotrixxLOL <ncusnir43@gmail.com>
2022-04-15 20:14:19 +0200

    some meta-tags added

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,51 @@
 import React from 'react';
 import Header from './Header';
+import Head from 'next/head'
+
+const titleMt = "Aprende computación hoy - RotrixX Blog"
+const descriptionMt = "Aprender cosas nuevas es dificil, o no? Con RotrixX Blog puedes conseguir articulos de cualquier cosa como por ejemplo: Crear una pagina de status en menos de 5 minutos. Asi que, que esperas? Unete ya!"
 
 const Layout = ({ children }) => (
   <>
+    <Head>
+        <title>{titleMt}</title>
+        <meta
+          name="title"
+          content={titleMt}
+        />
+        <meta
+          name="description"
+          content={descriptionMt}
+        />
+        <meta
+          name="og:description"
+          content={descriptionMt}
+        />
+        <meta
+          name="twitter:description"
+          content={descriptionMt}
+        />
+        <meta
+            name="keywords"
+            content="aprender computacion, programacion, computacion, hacking, articulos programacion, programacion posts, curso javascript gratis, curso react gratis, academia de programacion, como crear una pagina de status en menos de 5 minutos"
+        />
+        <meta
+            name="og:keywords"
+            content="aprender computacion, programacion, computacion, hacking, articulos programacion, programacion posts, curso javascript gratis, curso react gratis, academia de programacion, como crear una pagina de status en menos de 5 minutos"
+        />
+        <meta
+            name="twitter:keywords"
+            content="aprender computacion, programacion, computacion, hacking, articulos programacion, programacion posts, curso javascript gratis, curso react gratis, academia de programacion, como crear una pagina de status en menos de 5 minutos"
+        />
+        <meta
+            name="og:color"
+            content="blue"
+        />
+        <meta
+            name="author"
+            content="@RotrixX"
+        />
+    </Head>
     <Header />
     {children}
   </>
--- pages/index.js
+++ pages/index.js
@@ -30,4 +30,3 @@ export async function getStaticProps() {
     props: { posts },
   };
 }
-
