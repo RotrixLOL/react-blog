18ccef8645217ecc8366f22d2ea9017651ec888b
RotrixxLOL <ncusnir43@gmail.com>
2022-04-17 13:33:23 +0200

    modified favicon config

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -17,7 +17,10 @@ const Layout = ({ children }) => (
 
         <title>{titleMt}</title>
         <meta name="language" content="ES"/>
-        <link rel="manifest" href="../manifest.json" />
+        <link rel="manifest" href="manifest.json" />
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
 
         <meta
           name="title"
