3a21036cf7c839af158fca2e62e957c698182d34
RotrixxLOL <ncusnir43@gmail.com>
2022-10-29 23:20:37 +0200

    correction for left arrow

--- util.js
+++ util.js
@@ -1,1 +1,22 @@
+import { Feed } from 'feed';
+
 export const graphCMSImageLoader = ({ src }) => src;
+
+export const genFeed = ({ type }) => {
+    const feed = new Feed({});
+    switch (type) {
+        case 'rss':
+            return feed.rss2();
+            break;
+        case 'atom':
+            return feed.atom1();
+            break;
+        case 'json':
+            return feed.json1();
+            break;
+    
+        default:
+            return feed.rss2();
+            break;
+    }
+};
